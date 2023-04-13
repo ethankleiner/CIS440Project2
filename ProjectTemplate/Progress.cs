06ea6720d34b2e93fed1dc8b6cd80a82477d0d5e
HUANG <huangyu-hsuan@huangyuuandembp.3500.dhcp.asu.edu>
2023-04-12 23:18:52 -0700

    added Progress class

---
+++ ProjectTemplate/Progress.cs
@@ -0,0 +1,15 @@
+namespace ProjectTemplate
+{
+    public class Progress
+    {
+        public int progressID;
+        public int menteeID;
+        public int courseID;
+        public bool check1;
+        public bool check2;
+        public bool check3;
+        public bool check4;
+        public bool check5;
+        public int progress;
+    }
+}
